51a752ce93b9d13168e6275dcccada74c4503b23
Erick Ochieng <erickochieng766@gmail.com>
2022-12-08 10:52:27 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -4,6 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    let likebtns = document.querySelectorAll(".like-glyph")
+    likebtns.forEach(likeHandler)
+})
+
+function likeHandler(glyph) {
+  glyph.addEventListener('click', likeCallback)
+}
+
+function toggleHeart(like) {
+  if(like.textContent === EMPTY_HEART) {
+    like.textContent = FULL_HEART
+    like.classList.add("activated-heart")
+  } else {
+    like.textContent = EMPTY_HEART
+    like.classList.remove("activated-heart")
+  }
+}
+
+function likeCallback(event) {
+  mimicServerCall()
+  .then(response => {
+    toggleHeart(event.target)
+  })
+  .catch(error => {
+    const modal = document.querySelector("#modal")
+    modal.classList.remove("hidden")
+    document.querySelector("#modal p").textContent = "Like Rejected"
+    setTimeout(()=> {
+      modal.classList.add("hidden")
+    }, 3000)
+  })
+}
 
 
 
